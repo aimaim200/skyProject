c97ecc34ed93f5f884d2d3cb3b0a031122bac67a
ADMIN <admin@ADMINs-MacBook-Pro.local>
2019-09-15 12:07:42 +0100

    Modified the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer">
-    <span>&copy; Kennedy Mumba @ 2017 </span>
+    <span>&copy; Kennedy Mumba @ 2019</span>
   </div>
 );
 export default Footer;
